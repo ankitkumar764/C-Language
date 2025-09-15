bb16624ff208cac5fa2cc7865141e36157c3ba9f
ankitkumar764 <ankit.kr.cg@gmail.com>
2025-09-15 11:20:49 +0530

    first

---
+++ Pattern-Print/que21.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+
+for(int i=1; i<=5; i++){
+    for(int j=1; j<=i-1; j++){
+        printf(" ");
+    }
+    for(int k=1; k<=6-i; k++){
+        printf("*");
+    }
+    printf("\n");
+}
+
+
+
+
+
+return 0;
+}
---
+++ Pattern-Print/que22.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+for(int i=1; i<=5; i++){
+    for(int j=1; j<=i-1; j++){
+        printf(" ");
+    }
+    for(int k=1; k<=6-i; k++){
+        printf("%d",k);
+    }
+    printf("\n");
+}
+
+
+
+
+    return 0;
+}
