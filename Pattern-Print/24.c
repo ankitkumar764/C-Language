56dc09b71d75f88ee5b29ecb687c1e6232bd9df4
ankitkumar764 <ankit.kr.cg@gmail.com>
2025-09-20 23:27:48 +0530

    third

---
+++ Pattern-Print/24.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=n-i+1; j++){
+            printf("%d",j);
+        }
+        for(int k=n-i; k>=1; k--){
+            printf("%d",k);
+        }
+        printf("\n");
+    }
+
+
+
+
+
+
+
+
+
+   
+    return 0;
+}
